f55767b8a08c797d19612a4fbfe07a9f0c3c9c8a
davidplatar <david.plata@hotmail.com>
2023-11-24 13:54:52 -0500

    add elements

--- main.js
+++ main.js
@@ -12,8 +12,26 @@ function buildBooks(books){
     for(const book of books){
         html.push(`
         <a href="#" class="col-4 mb-3 book">
-            <img src="" alt="" class="img-fluid">
+            <img src="${book.image}" alt="${book.title}" class="img-fluid">
         </a>`)
     }
     return html
 }
+
+
+// fetch
+// fetch('https://seussology.info/api/books')
+//     .then(response => response.json())
+//     .then(json => console.log(json))
+
+
+async function getBooks(){
+    const response = await fetch('https://seussology.info/api/books')
+    const data = await response.json()
+
+    console.log(data)
+    const html = buildBooks(data)
+    $books.innerHTML = html.join('')
+}
+
+getBooks()
