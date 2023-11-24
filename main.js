55bbab24346de3d0e29f19c77aeaed2e6db45282
davidplatar <david.plata@hotmail.com>
2023-11-24 14:11:45 -0500

    adds getBook

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const $books =  document.getElementById('books')
 const $store = document.getElementById('store')
 
 
+let books = null
+
 
 // function create the elements
 function buildBooks(books){
@@ -28,10 +30,34 @@ function buildBooks(books){
 async function getBooks(){
     const response = await fetch('https://seussology.info/api/books')
     const data = await response.json()
-
+    // books = data
     console.log(data)
     const html = buildBooks(data)
     $books.innerHTML = html.join('')
 }
 
 getBooks()
+
+//Fetch a single book
+
+async function getBook(id){
+    const response = await fetch('https://seussology.info/api/books/' + id)
+    const book = await response.json() 
+
+    $books.innerHTML = `
+    <div class="row">
+        <a href="#" class="col-6 mb-3 book">
+            <img src="${book.image}" alt="" class="img-fluid">
+        </a>
+        <div class="col-6">
+            <h1>${book.title}</h1>
+            <p>${book.description}</p>
+            <button class="back btn btn-secondary">Back</button>
+            <button class="save btn btn-secondary">+</button>
+            <button class="remove btn btn-secondary">-</button>
+
+        </div>
+    </div>
+    `
+
+}
